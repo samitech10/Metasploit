0e15ad4c941e56f3ca303f8ad01e585b8533e9fd
samitech10 <55376371+samitech10@users.noreply.github.com>
2019-09-26 10:02:17 +0530

    Revert "Update ftpscan.rb"
    This reverts commit 4a4121375ef31979ba5d6fb02c3ca5c4d84bb940.

--- Auxiliary/ftpscan.rb
+++ Auxiliary/ftpscan.rb
@@ -6,7 +6,7 @@ class MetasploitModule < Msf::Auxiliary
     super(
       'Name'        => 'FTP Version Scanner Customized Module',
       'Description' => 'Detect FTP Version from the Target',
-      'Author'      => 'Mohd.Samiuzzaman',
+      'Author'      => 'Nipun Jaswal',
       'License'     =>  MSF_LICENSE
     )
 
